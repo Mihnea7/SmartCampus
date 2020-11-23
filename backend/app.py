11a2098a6c2d2aad6c999156c54c73dd675978fc
Mihnea Maldaianu <46821487+Mihnea7@users.noreply.github.com>
2020-11-23 23:01:11 +0000

    [#27] Enable Flask to read connection string from terminal (#28)
    * Important for setting up CI so that the app can connect to the DB

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,21 @@
+import sys
 from flask import Flask, jsonify
 import pymongo
 import configparser
 
 app = Flask(__name__)
-config = configparser.ConfigParser()
-config.read("config_flask.ini")
-print(config["DATABASE"]["CONSTRING"])
-client = pymongo.MongoClient(str(config["DATABASE"]["CONSTRING"]))
+client = None
+
+if len(sys.argv) == 1:
+    config = configparser.ConfigParser()
+    config.read("config_flask.ini")
+    print(config["DATABASE"]["CONSTRING"])
+    client = pymongo.MongoClient(str(config["DATABASE"]["CONSTRING"]))
+
+
+elif len(sys.argv) > 1:
+    client = pymongo.MongoClient(sys.argv[1])
+
 db = client["SCDB"]
 
 
