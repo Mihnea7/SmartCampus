9d52f9d1c9c07452fce4db43a267f165fa368641
Mihnea Maldaianu <maldaianumihnea@yahoo.com>
2020-12-15 20:06:16 +0000

    [#26] Create view for Boyd Orr Building
    * Enabled CORS with flask-cors cross_origin
    * Added a view get_boyd_orr_sensors that returns a list of all sensors in the BoydOrr collection
    * Created pytests for the view

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 import sys
 from flask import Flask, jsonify
+from flask_cors import cross_origin
 import pymongo
 import configparser
 
 app = Flask(__name__)
+
 client = None
 
 if len(sys.argv) == 1:
@@ -19,18 +21,29 @@ elif len(sys.argv) > 1:
 db = client["SCDB"]
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
     return "No data to show"
 
 
-@app.route("/test-temp")
+@app.route("/test-temp", methods=["GET"])
+@cross_origin()
 def get_test_temperatures():
     all_temperature = []
     for elem in db["TempSens"].find():
         elem["_id"] = str(elem["_id"])
         all_temperature.append(elem)
     return jsonify(all_temperature)
+
+
+@app.route("/boyd-orr", methods=["GET"])
+@cross_origin()
+def get_boyd_orr_sensors():
+    all_sensors = []
+    for elem in db["BoydOrr"].find():
+        elem["_id"] = str(elem["_id"])
+        all_sensors.append(elem)
+    return jsonify(all_sensors)
 
 
 if __name__ == "__main__":
--- backend/test_flask.py
+++ backend/test_flask.py
@@ -24,3 +24,15 @@ def test_test_temperature(app, client):
     assert rv.status_code == 200
     expected = json.loads(rv.get_data(as_text=True))
     assert len(expected) == 5
+
+
+def test_get_boyd_orr_sensors(app, client):
+    rv = client.get('/boyd-orr')
+    assert rv.status_code == 200
+    expected = json.loads(rv.get_data(as_text=True))
+    assert len(expected) > 0
+    first_item = expected[0]
+    assert first_item["name"] is not None
+    assert type(first_item["level"]) is str
+    assert type(first_item["current"]) is dict
+    assert type(first_item["history"]) is list
