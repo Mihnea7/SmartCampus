c023c67bf58e3535b388e22053beb2d46934b30d
Mihnea Maldaianu <46821487+Mihnea7@users.noreply.github.com>
2021-01-12 00:57:12 +0000

    [#31] Create DisplayBox Component (#32) (#34)
    * Added DisplayBox.js and DisplayBox.css

--- frontend/src/components/BoydOrr.js
+++ frontend/src/components/BoydOrr.js
@@ -1,35 +1,37 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 import DisplayBox from "./DisplayBox.js";
+import GraphComponent from "./GraphComponent.js";
+
 
 export default class BoydOrr extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            sensors : []
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      sensors: [],
+    };
+  }
 
-    componentDidMount() {
-        axios.get("http://127.0.0.1:5000/boyd-orr")
-      .then(res => {
-        const all_sensors = res.data;
-        console.log(all_sensors);
-        this.setState({ sensors : all_sensors });
-      })
-      
-    }
+  componentDidMount() {
+    axios.get("http://127.0.0.1:5000/boyd-orr").then((res) => {
+      const all_sensors = res.data;
+      this.setState((state) => {
+        return { sensors: all_sensors };
+      });
+    });
+  }
 
-
-    render () {
-        
-        return (
-            <div>
-                
-            {this.state.sensors.map(sensor =>
-                <DisplayBox name = {sensor["name"]} currentData = {sensor["current"]} />
-            )}   
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        {this.state.sensors.map((sensor) => (
+          <div>
+            <DisplayBox name={sensor["name"]} currentData={sensor["current"]} />
+            <GraphComponent seriesData={sensor["history"]} /> 
+            
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
