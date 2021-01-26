34e4af1d3227cc368e4d6d4fbce9b18fc85d1f3a
Mihnea Maldaianu <46821487+Mihnea7@users.noreply.github.com>
2021-01-26 12:54:59 +0000

    [#44] Added welcome page (#45)
    * Added a simple welcome page as landing page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,14 @@
-import Menu from "./Menu.js";
 import BoydOrr from "./BoydOrr.js";
 import Header from "./Header.js";
 import { Switch, Route } from "react-router-dom";
+import Welcome from "./Welcome.js";
 
 function App() {
   return (
     <main>
       <Header />
       <Switch>
+        <Route path="/" component={Welcome} exact />
         <Route path="/boyd-orr" component={BoydOrr} />
         <Route component={Error} />
       </Switch>
---
+++ frontend/src/components/Welcome.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class Welcome extends React.Component {
+  render() {
+    return (
+        <div>
+          <center>
+      <h5>Welcome to UoG Smart Campus Dashboard!</h5>
+      <p> To navigate the site, select the sensors you would like to see from the Menu on the 
+          left.
+      </p>
+      </center>
+      </div>
+    );
+  }
+}
