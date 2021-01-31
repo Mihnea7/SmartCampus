1642d99f4b6d4b4d7b57ae4d76cb2576b2782bf3
Mihnea Maldaianu <46821487+Mihnea7@users.noreply.github.com>
2021-01-31 21:33:00 +0000

    [#56] Make Level collapsible (#58)
    * Used react-collapsible to add said functionality to Level
    * Created Level.css and moved display-sensor to it from DisplayBox.css
    * Updated Cypress tests

--- frontend/cypress/integration/library.spec.js
+++ frontend/cypress/integration/library.spec.js
@@ -1,26 +1,36 @@
 describe("Library Test", function () {
-    it("Test arbitrary Display Box", function () {
-      cy.visit("/library");
-  
-      cy.get(".displaybox")
+  it("Test arbitrary Display Box", function () {
+    cy.visit("/library");
+
+    cy.get(".displaybox")
       .should("contain", "Current value")
       .and("contain", "time")
       .and("contain", "value");
-    });
-  
-    it("Test the number of display boxes", function () {
-      let tested = cy.get(".displaybox").its("length");
-      tested.should("eq", 4);
-    });
-
-    it("Test the number of graphs", function () {
-        let tested = cy.get("svg").its("length");
-        tested.should("eq", 4);
-      });
-  
-    it("Test if correct number of levels is displayed", function () {
-      let tested = cy.get(".level").its("length");
-      tested.should("eq", 2);
-    });
   });
-  
+
+  it("Test the number of display boxes", function () {
+    let tested = cy.get(".displaybox").its("length");
+    tested.should("eq", 4);
+  });
+
+  it("Test the number of graphs", function () {
+    let tested = cy.get("svg").its("length");
+    tested.should("eq", 4);
+  });
+
+  it("Test if correct number of levels is displayed", function () {
+    let tested = cy.get(".level").its("length");
+    tested.should("eq", 2);
+  });
+
+  it("Test if level is collapsed", function () {
+    cy.reload();
+    cy.get(".display-sensor").should("not.exist");
+  });
+
+  it("Test if level is active", function () {
+    cy.reload();
+    cy.get(".collapsible-text").first().click();
+    cy.get(".display-sensor").should("exist");
+  });
+});
--- frontend/src/components/Level.js
+++ frontend/src/components/Level.js
@@ -1,26 +1,44 @@
 import React from "react";
 import DisplayBox from "./DisplayBox.js";
 import GraphComponent from "./GraphComponent.js";
+import { Collapse } from "react-collapse";
 import "./DisplayBox.css";
+import "./Level.css";
 
 export default class Level extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       sensors: this.props.sensors,
+      opened: false,
     };
+    this.toggleCollapse = this.toggleCollapse.bind(this);
+  }
+
+  toggleCollapse() {
+    this.setState((prevState) => ({
+      opened: !prevState.opened,
+    }));
   }
 
   render() {
     return (
       <div class="level">
-        <h2><center>Level {this.props.sensors[0].level}</center></h2><hr/>
-        {this.state.sensors.map((sensor) => (
-          <div class="displaySensor">
-            <DisplayBox name={sensor["name"]} currentData={sensor["current"]} />
-            <GraphComponent seriesData={sensor["history"]} />
-          </div>
-        ))}
+        <h2 class="collapsible-text" onClick={this.toggleCollapse}>
+          <center>Level {this.props.sensors[0].level}</center>
+        </h2>
+        <hr />
+        <Collapse isOpened={this.state.opened}>
+          {this.state.sensors.map((sensor) => (
+            <div class="display-sensor">
+              <DisplayBox
+                name={sensor["name"]}
+                currentData={sensor["current"]}
+              />
+              <GraphComponent seriesData={sensor["history"]} />
+            </div>
+          ))}
+        </Collapse>
       </div>
     );
   }
