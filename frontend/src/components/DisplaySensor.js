834fef1091095790a6981f970b3f32af8d059ebb
Mihnea Maldaianu <maldaianumihnea@yahoo.com>
2021-02-02 18:48:52 +0000

    [#57] Make Display Box collapsible
    * Refactored components: created new DisplaySensor that renders DisplayBox and GraphComponent
    * Created CSS file for DisplaySensor and edited Level.css to reflect the new themes for the collapsible component
    * Tweaked styles in Header and DisplayBox to componsate for the refactorings

---
+++ frontend/src/components/DisplaySensor.js
@@ -0,0 +1,42 @@
+import React from "react";
+import DisplayBox from "./DisplayBox.js";
+import GraphComponent from "./GraphComponent.js";
+import { Collapse } from "react-collapse";
+import "./DisplaySensor.css";
+
+export default class Level extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      opened: false,
+    };
+    this.toggleCollapse = this.toggleCollapse.bind(this);
+  }
+
+  toggleCollapse() {
+    this.setState((prevState) => ({
+      opened: !prevState.opened,
+    }));
+  }
+
+  render() {
+    return (
+      <div class="display-sensor">
+        <div class="formal-name">
+          <h3 class="sensorbox-collapsible-text" onClick={this.toggleCollapse}>
+            <center>{this.props.formal}</center>
+          </h3>
+        </div>
+        <Collapse isOpened={this.state.opened}>
+          <DisplayBox
+            name={this.props.name}
+            currentData={this.props.currentData}
+          />
+          <div class="graph-box">
+            <GraphComponent seriesData={this.props.seriesData} />
+          </div>
+        </Collapse>
+      </div>
+    );
+  }
+}
