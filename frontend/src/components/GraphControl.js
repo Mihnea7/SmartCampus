a7039d0e2b952f33673ee2acec325bf0ada18f85
Mihnea Maldaianu <maldaianumihnea@yahoo.com>
2021-02-04 17:53:53 +0000

    [#55] Implement full screen functionality for Graphs
    * Added a fullscreen icon under the graph that, once clicked, will expand it into a popup
    * Added more control variables for graphs (yMaxVal, height, width)

---
+++ frontend/src/components/GraphControl.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Popup from "reactjs-popup";
+import GraphComponent from "./GraphComponent";
+import fullscreen from "../image/fullscreen.svg";
+import "./GraphControl.css"
+
+export default class GraphControl extends React.Component {
+  render() {
+    return (
+      <Popup
+        trigger={
+          <img
+            src={fullscreen}
+            alt="Fullscreen"
+            ref={(elm) => {
+              this.image = elm;
+            }}
+          />
+        }
+        position="right center"
+        closeOnDocumentClick
+        modal
+        nested
+      >
+          <div class="popup-box">
+        <GraphComponent
+          seriesData={this.props.seriesData}
+          yMaxVal={this.props.yMaxVal}
+          height={this.props.height}
+        />
+        </div>
+      </Popup>
+    );
+  }
+}
