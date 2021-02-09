ec995414d3802323f4a2524908cd252c759416df
Mihnea Maldaianu <46821487+Mihnea7@users.noreply.github.com>
2021-02-09 11:29:10 +0000

    [#73] Add University Logo to Header (#76)
    * Added logo image to image directory
    * Added img to Header.js
    * Added logo test
    * Created Header.css for improved styling

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Menu from "./Menu.js";
+import logo from "../image/uni-logo.png";
+import "./Header.css";
+
 export default class Header extends React.Component {
   render() {
     return (
-      <div>
+      <div class="header-div">
+        <img
+            src={logo}
+            alt="Logo"
+            class="logo-image"
+          />
         <Menu />
-        <h1 style={{ textAlign: "center" }}> Smart Campus Dashboard</h1>
+          <h1 class="header-text"> Smart Campus Dashboard</h1>
+          
       </div>
     );
   }
