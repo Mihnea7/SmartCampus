ee0992db2d54d313f58c46e23cb2ab2fb95e637d
Mihnea Maldaianu <maldaianumihnea@yahoo.com>
2021-02-09 14:35:07 +0000

    [#69] Add OutsideSensor component
    * Added OutsideSensors.js with corresponding test file

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header.js";
 import { Switch, Route } from "react-router-dom";
 import Welcome from "./Welcome.js";
 import Library from "./Library.js";
+import OutsideSensors from "./OutsideSensors.js";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" component={Welcome} exact />
         <Route path="/boyd-orr" component={BoydOrr} />
         <Route path="/library" component={Library} />
+        <Route path="/outside-sensors" component={OutsideSensors} />
         <Route component={Error} />
       </Switch>
     </main>
