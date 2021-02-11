48db7fc4beb521dd1dbb07fbb83026eaa2fb932d
Mihnea Maldaianu <maldaianumihnea@yahoo.com>
2021-02-11 00:02:08 +0000

    [#82] Display Push Pins sensor on select
    * Created SingleSensorPage component to display a single sensor based on URL parameters
    * Added the above mentioned component to the React Router

--- frontend/src/components/OutsideSensors.js
+++ frontend/src/components/OutsideSensors.js
@@ -9,47 +9,65 @@ export default class Header extends React.Component {
     super(props);
     this.state = {
       sensors: [],
-      pins: [],
+      selectedSensor: "6020441c6105ef7c424d56ce",
     };
 
     this.createPins = this.createPins.bind(this);
+    this.handleSelectSensor = this.handleSelectSensor.bind(this);
   }
 
   componentDidMount() {
     axios.get("http://127.0.0.1:5000/outside-sensors").then((res) => {
       const all_sensors = res.data;
       this.setState((state) => {
-        return { sensors: all_sensors };
+        return { sensors: all_sensors, sens: all_sensors[0] };
       });
     });
   }
 
   createPins() {
     const pinsArray = [];
-    this.state.sensors.forEach(sensor =>  {
+    this.state.sensors.forEach((sensor) => {
       const pinSensor = {
+        _id: sensor["_id"],
         location: [sensor["lat"], sensor["long"]],
         option: {
           color: "red",
         },
+        addHandler: {
+          type: "click",
+          callback: () => this.handleSelectSensor(sensor["_id"]),
+        },
       };
       pinsArray.push(pinSensor);
     });
-    console.log(pinsArray);
     return pinsArray;
   }
 
+  handleSelectSensor(sensorName) {
+    this.setState({
+      selectedSensor: sensorName,
+    });
+    window.open("/outside-sensor?sensorid=" + sensorName);
+  }
+
   render() {
+    console.log(this.state.sens);
     return (
-      <div class="map-container">
-        {this.createPins().length === 0 ? false: <ReactBingmaps
-          bingmapKey={config.MAP_API_KEY}
-          center={[55.872422, -4.28857]}
-          navigationBarMode={"compact"}
-          zoom={17}
-          pushPins={this.createPins()}
-        />}
-        
+      <div>
+        <div class="map-container">
+          {this.createPins().length === 0 ? (
+            false
+          ) : (
+            <ReactBingmaps
+              bingmapKey={config.MAP_API_KEY}
+              center={[55.872422, -4.28857]}
+              navigationBarMode={"compact"}
+              zoom={17}
+              pushPins={this.createPins()}
+            />
+          )}
+        </div>
       </div>
     );
   }
