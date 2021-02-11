543cd42f36c45d2f56887cea672d7c767014e37f
Mihnea Maldaianu <maldaianumihnea@yahoo.com>
2021-02-11 19:35:09 +0000

    [#88] Added return to Welcome from Header
    * Clicking the logo or text in the header now results in the user being redirected to the main page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,18 @@
 import React from "react";
 import Menu from "./Menu.js";
 import logo from "../image/uni-logo.png";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 export default class Header extends React.Component {
   render() {
     return (
       <div class="header-div">
-        <img
-            src={logo}
-            alt="Logo"
-            class="logo-image"
-          />
+        <Link to="/" style={{ color: "inherit", textDecoration: "inherit" }}>
+          <img src={logo} alt="Logo" class="logo-image" />
+          <h1 class="header-text">Smart Campus Dashboard</h1>
+        </Link>
         <Menu />
-          <h1 class="header-text"> Smart Campus Dashboard</h1>
-          
       </div>
     );
   }
