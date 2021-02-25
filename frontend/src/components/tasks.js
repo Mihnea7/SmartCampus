3dcaccc28c7cc83072d6c9aa04dbd3c4a5625774
Mihnea Maldaianu <maldaianumihnea@yahoo.com>
2021-02-25 12:04:05 +0000

    [#135] Created task config file
    * Created tasks.js that exports an object storing all tasks text and answers

---
+++ frontend/src/components/tasks.js
@@ -0,0 +1,33 @@
+export default tasks = {
+  0: {
+    text: "How many people are currently in the Boyd Orr 222 Lecture Theatre?",
+    ans: 57,
+  },
+  1: {
+    text: "What is the current temperature in the Library on Level 2?",
+    ans: 19.6,
+  },
+  2: {
+    text:
+      "How many free parking spaces are there in the Main Building Parking Lot now?",
+    ans: 9,
+  },
+  3: {
+    text:
+      "What is the total current power consumption of the Boyd Orr Building?",
+    ans: 45190878,
+  },
+  4: {
+    text:
+      "Comparing 1) BO715 to 2) BO1028, which one can we say is, on average, the busiest? Answer either 1 or 2.",
+    ans: 2,
+  },
+  5: {
+    text: "What is the percentage of seats currently occupied in BO715?",
+    ans: 18.75,
+  },
+  6: {
+      text:"How many people are in Boyd Orr Lecture Theatre 221 on 21st November at 1:30pm?",
+      ans:0
+  }
+};
