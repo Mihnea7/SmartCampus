79b246c0f58c017bbae0c4dfc5eb10897fee3f6b
Mihnea Maldaianu <46821487+Mihnea7@users.noreply.github.com>
2021-03-01 13:06:59 +0000

    [#154] Refactored Comparator component (#155)
    Closes #154
    * Added ComparatorContainer that renders Comparator

--- frontend/src/components/Comparator.js
+++ frontend/src/components/Comparator.js
@@ -1,39 +1,11 @@
 import React from "react";
-import axios from "axios";
 import GraphComponent from "./GraphComponent.js";
 
 export default class Comparator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sensor: [],
-    };
-  }
-  componentDidMount() {
-    const qParams = this.props.location.search;
-    const id_list = qParams.split("&");
-    const collection = id_list.splice(0, 1).join("");
-    console.log(collection);
-    id_list.forEach((id) => {
-      console.log(id);
-      axios
-        .get("http://127.0.0.1:5000/sensor" + collection + "&" + id)
-        .then((res) => {
-          const all_sensor = res.data;
-          const add_state = this.state.sensor;
-          add_state.push(all_sensor);
-          this.setState((state) => {
-            return { sensor: add_state };
-          });
-        });
-    });
-  }
-
   render() {
-    console.log(this.state.sensor);
     return (
       <div>
-        {this.state.sensor.map((sensor) => {
+        {this.props.sensor.map((sensor) => {
           return (
             <center>
               <h3>{sensor.formal}</h3>
---
+++ frontend/src/containers/ComparatorContainer.js
@@ -0,0 +1,33 @@
+import React from "react";
+import axios from "axios";
+import Comparator from "../components/Comparator";
+
+export default class ComparatorContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sensor: [],
+    };
+  }
+  componentDidMount() {
+    const qParams = this.props.location.search;
+    const id_list = qParams.split("&");
+    const collection = id_list.splice(0, 1).join("");
+    id_list.forEach((id) => {
+      console.log(id);
+      axios
+        .get("http://127.0.0.1:5000/sensor" + collection + "&" + id)
+        .then((res) => {
+          const all_sensor = res.data;
+          const add_state = this.state.sensor;
+          add_state.push(all_sensor);
+          this.setState((state) => {
+            return { sensor: add_state };
+          });
+        });
+    });
+  }
+  render() {
+      return <Comparator sensor={this.state.sensor}/>
+  }
+}
