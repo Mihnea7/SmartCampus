451a9b758cc4cce3c23b248bc5d932e847eeeec1
Mihnea Maldaianu <maldaianumihnea@yahoo.com>
2021-03-01 12:47:39 +0000

    [#152] Closes #152 * Added SingleSensorPageContaner.js that renders SinglePageSensor

--- frontend/src/components/SingleSensorPage.js
+++ frontend/src/components/SingleSensorPage.js
@@ -1,29 +1,12 @@
 import React from "react";
-import axios from "axios";
 import DisplaySensor from "./DisplaySensor.js";
 export default class SingleSensorPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sensor: [],
-    };
-  }
-  componentDidMount() {
-    let qParams = this.props.location.search;
-    console.log(qParams);
-    axios.get("http://127.0.0.1:5000/sensor" + qParams).then((res) => {
-      const all_sensor = res.data;
-      console.log(all_sensor);
-      this.setState((state) => {
-        return { sensor: [all_sensor] };
-      });
-    });
-  }
+  
   render() {
 
     return (
         <div>
-        {this.state.sensor.map((sensor) => (
+        {this.props.sensor.map((sensor) => (
             <DisplaySensor
               name={sensor["name"]}
               formal={sensor["formalName"]}
